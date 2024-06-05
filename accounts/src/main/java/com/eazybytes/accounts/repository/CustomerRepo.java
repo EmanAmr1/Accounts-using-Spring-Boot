e6a0edc5adae38eacec0e24596cc0da5451f77d9
EmanAmr1 <emanamr543@gmail.com>
2024-06-05 12:33:58 +0300

    create new account

--- accounts/src/main/java/com/eazybytes/accounts/repository/CustomerRepo.java
+++ accounts/src/main/java/com/eazybytes/accounts/repository/CustomerRepo.java
@@ -1,9 +1,10 @@
 package com.eazybytes.accounts.repository;
 
 
+import com.eazybytes.accounts.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepo extends JpaRepository<CustomerRepo,Long> {
+public interface CustomerRepo extends JpaRepository<Customer,Long> {
 }
--- accounts/src/main/java/com/eazybytes/accounts/service/impl/IAccountServiceImpl.java
+++ accounts/src/main/java/com/eazybytes/accounts/service/impl/IAccountServiceImpl.java
@@ -1,20 +1,49 @@
 package com.eazybytes.accounts.service.impl;
 
+import com.eazybytes.accounts.constants.AccountsConstants;
 import com.eazybytes.accounts.dto.CustomerDto;
+import com.eazybytes.accounts.entity.Accounts;
+import com.eazybytes.accounts.entity.Customer;
+import com.eazybytes.accounts.mapper.CustomerMapper;
 import com.eazybytes.accounts.repository.AccountsRepo;
 import com.eazybytes.accounts.repository.CustomerRepo;
 import com.eazybytes.accounts.service.IAccountsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 @Service
 @AllArgsConstructor
 public class IAccountServiceImpl implements IAccountsService {
 
+
     private AccountsRepo accountsRepo;
+
     private CustomerRepo customerRepo;
     @Override
     public void createAccount(CustomerDto customerDto) {
 
+        Customer customer= CustomerMapper.mapToCustomer(customerDto ,new Customer());
+
+        Customer savedCustomer= customerRepo.save(customer);
+
+        accountsRepo.save(CreateNewAccount(savedCustomer));
+
+    }
+
+
+
+    private Accounts CreateNewAccount (Customer customer){
+
+        Accounts newAccount =new Accounts();
+        newAccount.setCustomerId(customer.getCustomerId());
+
+        long randomAccNumber =1000000000l +new Random().nextInt(900000000);
+
+        newAccount.setAccountNumber(randomAccNumber);
+        newAccount.setAccountType(AccountsConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+        return newAccount;
     }
 }
