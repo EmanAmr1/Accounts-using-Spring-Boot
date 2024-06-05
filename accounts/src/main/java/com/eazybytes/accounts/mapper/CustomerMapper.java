de22de50f7a37bd9b6e844bc3659201bf325e8ca
EmanAmr1 <emanamr543@gmail.com>
2024-06-05 12:16:28 +0300

    customer and account mapper

---
+++ accounts/src/main/java/com/eazybytes/accounts/mapper/CustomerMapper.java
@@ -0,0 +1,21 @@
+package com.eazybytes.accounts.mapper;
+
+import com.eazybytes.accounts.dto.CustomerDto;
+import com.eazybytes.accounts.entity.Customer;
+
+public class CustomerMapper {
+
+    public static CustomerDto mapToCustomerDto( Customer customer ,CustomerDto customerDto){
+        customerDto.setMobileNumber(customer.getMobileNumber());
+        customerDto.setName(customer.getName());
+        customerDto.setEmail(customer.getEmail());
+        return customerDto;
+    }
+
+    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer){
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        customer.setName(customerDto.getName());
+        customer.setName(customerDto.getEmail());
+        return customer;
+    }
+}
